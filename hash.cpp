32e5262406837a00024a1f79e68be94dd732111e
paulynaa <147087833+paulynaa@users.noreply.github.com>
2024-09-25 23:16:14 +0300

    Add files via upload

--- hash.cpp
+++ hash.cpp
@@ -8,6 +8,93 @@
 #include <chrono>
 using namespace std;
 
+
+void wypisywanie(char H[]){
+    int x=0;
+    for(int j=0;j<4;j++){
+        for(int i=0;i<16;i++)
+        {
+            cout<<H[x]<<" ";x++;
+        }
+        cout<<endl;
+    }
+}//israsimas
+
+void robienie64Key(char H[],char b[],int q){
+    int x=0,qq=q;
+    for(int j=0;j<4;j++){
+        for(int i=0;i<16;i++)
+        {
+            H[x]=b[(i+qq)%16];x++;
+        }
+        qq+=qq;
+    }
+    if(q>64){
+    q=q-64;
+    qq=q;
+    x=0;
+    for(int j=0;j<q/16;j++){
+        for(int i=0;i<q/4;i+=q/16)
+        {
+            H[x]=b[(qq^2)%16];x++;
+        }
+        qq+=qq;
+    }
+    q=q+64;
+    }
+
+}//nadajim znaczenia dla H
+
+void cope(char H[],char L[]){
+    for(int i=0;i<64;i++){
+        L[i]=H[i];
+    }
+}
+
+void Haszinimas1(char H[],char b[],int q){
+    int qq=q;
+    if(q>64)
+        {qq=q-64+1;}
+    for(int i=0;i<64;i=i+qq+1){
+        H[i]=b[i%16];
+    }
+}
+void Haszinimas2(char H[],char b[],int q){
+    int qq=q%16;
+    for(int i=0;i<64;i+=qq+1){
+        H[i]=b[i%16];
+    }
+}
+
+void Haszinimas3(char H[],char b[],int q,char L[]){
+    int qq=q;
+    if(q>64){qq=q-64+1;}
+    for(int i=0;i<64;i+=qq+1){
+        if(2*i<64){H[i]=L[2*i];}
+        else{H[i]=L[(2*i)%64];}
+    }
+}
+
+void HASZHASZ(int f,char N[],char H[],char h[],char b[],char L[]){
+    for(int i=0;i<64;i++){
+        N[i]=H[i];
+    }
+    int q;
+    for(int i=0;i<64;i++){
+        q =(int)N[i];
+    Haszinimas1(H,b,q);
+    Haszinimas2(H,b,q);
+    cope(H,L);
+    Haszinimas3(H,b,q,L);
+    }
+    q=(int)h[f];
+    Haszinimas1(H,b,q);
+    Haszinimas2(H,b,q);
+    cope(H,L);
+    Haszinimas3(H,b,q,L);
+}
+
+
 string failoskaitytuvas(string failopav) {
     ifstream failas(failopav);
     if (!failas.is_open()) {
@@ -100,87 +187,6 @@ void konstitucija(string konst) {
 
     kons.close();
 }
-
-
-void wypisywanie(char H[]){
-    int x=0;
-for(int j=0;j<4;j++){
-for(int i=0;i<16;i++)
-{
-    cout<<H[x]<<" ";x++;
-}
-cout<<endl;
-}
-}//israsimas
-void robienie64Key(char H[],char b[],int q){
-int x=0,qq=q;
-for(int j=0;j<4;j++){
-for(int i=0;i<16;i++)
-{
-    H[x]=b[(i+qq)%16];x++;
-}
-qq+=qq;
-}
-if(q>64){
-q=q-64;
-qq=q;
-x=0;
-for(int j=0;j<q/16;j++){
-for(int i=0;i<q/4;i+=q/16)
-{
-    H[x]=b[(qq^2)%16];x++;
-}
-qq+=qq;
-}
-q=q+64;
-}
-
-}//nadajim znaczenia dla H
-void cope(char H[],char L[]){
-    for(int i=0;i<64;i++){
-        L[i]=H[i];
-    }
-}
-void Haszinimas1(char H[],char b[],int q){
-int qq=q;
-if(q>64){qq=q-64+1;}
-for(int i=0;i<64;i=i+qq+1){
-H[i]=b[i%16];
-}
-}
-void Haszinimas2(char H[],char b[],int q){
-int qq=q%16;
-for(int i=0;i<64;i+=qq+1){
-H[i]=b[i%16];
-}
-}
-void Haszinimas3(char H[],char b[],int q,char L[]){
-int qq=q;
-if(q>64){qq=q-64+1;}
-for(int i=0;i<64;i+=qq+1){
-if(2*i<64){H[i]=L[2*i];}
-else{H[i]=L[(2*i)%64];}
-}
-}
-void HASZHASZ(int f,char N[],char H[],char h[],char b[],char L[]){
-for(int i=0;i<64;i++){
-    N[i]=H[i];
-}
-int q;
-for(int i=0;i<64;i++){
-q =(int)N[i];
-Haszinimas1(H,b,q);
-Haszinimas2(H,b,q);
-cope(H,L);
-Haszinimas3(H,b,q,L);
-}
-q=(int)h[f];
-Haszinimas1(H,b,q);
-Haszinimas2(H,b,q);
-cope(H,L);
-Haszinimas3(H,b,q,L);
-}
-
 
 
 int main() {
